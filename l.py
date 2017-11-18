2a60f8fe02f167c5acb4a2e412216c9a84ab9e3a
Egor Garin <garinegor@gmail.com>
2017-11-18 20:42:16 +0300

    bot

--- l.py
+++ l.py
@@ -20,6 +20,7 @@ def send_photo(message):
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, "чтобы получить фото с домофона, нажми /photo")
+    print(message.chat.id)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
