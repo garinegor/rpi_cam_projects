214a69c9696e81f5f387f074f2f218f47be5711d
Egor Garin <garinegor@gmail.com>
2017-11-25 23:29:54 +0300

    kkk

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(18)
     if input_state == False:
-        bot.send_message(chat_id=202226598, "кто-то пришел")
+        bot.send_message(202226598, "кто-то пришел")
         time.sleep(0.2)
 
 if __name__ == '__main__':
