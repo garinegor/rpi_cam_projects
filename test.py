908e11aa13f662890673e2d581afee51eda67760
Egor Garin <garinegor@gmail.com>
2017-11-25 23:29:08 +0300

    kkk

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from time import sleep
 import config,telebot,json
 from telebot import types
+import RPi.GPIO as GPIO
 
 bot = telebot.TeleBot(config.token)
 users=[202226598, 56345999]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     input_state = GPIO.input(18)
