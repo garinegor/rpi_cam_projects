1e7a29cd59fe6705b8e7f94dd66e99ae4cdf66ea
Egor Garin <garinegor@gmail.com>
2017-12-10 20:10:47 +0300

    added images folder

--- fr.py
+++ fr.py
@@ -10,13 +10,13 @@ faces={'george':['8cb744fc-734d-48c2-8633-af34f3d5ff4e', 'dea5a71e-a2ae-4da3-be2
 # print(faces)
 
 def compare(name,path):
+	to_r=False
 	faces_result = CF.face.detect(path)
 	if len(faces_result)==1:
 		new_face=faces_result[0]['faceId']
 		result=CF.face.group(faces[name]+[new_face])
 		if new_face in result['groups'][0]:
-			return True
-	else:
-		return False
+			to_r=True
+	return to_r
 
-print(compare('george','./new_photo.jpg'))
+print(compare('george','./images/image4.jpg'))
